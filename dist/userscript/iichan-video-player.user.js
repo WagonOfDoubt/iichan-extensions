673ec528da0ebf90e0c271a2c1dc8d8beb0481db
WagonOfDoubt <WagonOfDoubt@users.noreply.github.com>
2019-05-10 00:12:19 +0300

    Bump version

--- dist/userscript/iichan-video-player.user.js
+++ dist/userscript/iichan-video-player.user.js
@@ -2,15 +2,13 @@
 // @name         [IIchan] Video player
 // @namespace    http://iichan.hk/
 // @license      MIT
-// @version      0.5
+// @version      0.6
 // @description  Video players on thumbnail click
 // @icon         http://iichan.hk/favicon.ico
 // @updateURL    https://raw.github.com/WagonOfDoubt/iichan-extensions/master/dist/userscript/iichan-video-player.meta.js
 // @author       Mithgol
 // @match        http://nowere.net/*
 // @match        https://nowere.net/*
-// @match        http://iichan.hk/*
-// @match        https://iichan.hk/*
 // @grant        none
 // ==/UserScript==
 
