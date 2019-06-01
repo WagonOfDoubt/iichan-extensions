02618e8d89b491a39d40b3d481520f6885dd0ac2
WagonOfDoubt <WagonOfDoubt@users.noreply.github.com>
2019-06-01 23:35:33 +0300

    [configurator] Bump version

--- src/configurator/configurator.meta.js
+++ src/configurator/configurator.meta.js
@@ -2,7 +2,7 @@
 // @name         [IIchan] Configurator
 // @namespace    http://iichan.hk/
 // @license      MIT
-// @version      0.1
+// @version      0.2
 // @description  Configure scripts on iichan
 // @icon         https://iichan.hk/favicon.ico
 // @updateURL    https://raw.github.com/WagonOfDoubt/iichan-extensions/master/dist/userscript/iichan-configurator.meta.js
