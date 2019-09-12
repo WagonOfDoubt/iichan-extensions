28c4c5f04878cf101a4c4350e656493d36b2b4ea
WagonOfDoubt <WagonOfDoubt@users.noreply.github.com>
2019-09-12 03:18:17 +0300

    [hide-threads][expand-images] Bump versions

--- dist/userscript/iichan-expand-images.user.js
+++ dist/userscript/iichan-expand-images.user.js
@@ -2,7 +2,7 @@
 // @name         [IIchan] Expand images
 // @namespace    http://iichan.hk/
 // @license      MIT
-// @version      0.6
+// @version      0.7
 // @description  Expands images on click
 // @icon         http://iichan.hk/favicon.ico
 // @updateURL    https://raw.github.com/WagonOfDoubt/iichan-extensions/master/dist/userscript/iichan-expand-images.meta.js
